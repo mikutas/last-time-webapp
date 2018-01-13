32eca43bbcb90a12f7b0f534a005e08501f36580
tkms0106 <siberianexpress1991@gmail.com>
2018-01-13 20:00:15 +0900

    [update]ユーザーから属性、カラムの一部を削除
    OAuthによるログインに限定するため

---
+++ db/migrate/20180113104833_remove_name_and_password_digest_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveNameAndPasswordDigestFromUsers < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :users, :name, :string
+    remove_column :users, :password_digest, :string
+  end
+end
