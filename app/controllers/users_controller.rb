a7228cbca2ce6b2a6d9770490acca1516cc7449d
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-01-14 09:49:53 +0900

    [add]正しいユーザーかどうか確認するcorrect_userを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_user
 
   def show
     @user = User.find(params[:id])
@@ -13,5 +14,11 @@ class UsersController < ApplicationController
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
