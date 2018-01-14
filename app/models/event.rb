7535dcc9be078b4ff3c37bc08eb23d7252a29fa1
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-01-14 14:11:59 +0900

    [update]Event作成時に1つ目の関連するOccurredDateを自動的に作成するように変更

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,12 @@
 class Event < ApplicationRecord
+  after_save :create_first_date
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates_presence_of :user_id
   validates_presence_of :title
-  has_many :occurred_dates, dependent: :destroy
+  has_many :occurred_dates, dependent: :delete_all
+
+  def create_first_date
+    self.occurred_dates.create(created_at: Time.zone.now)
+  end
 end
