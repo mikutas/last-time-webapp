e3e3f4bdd8b7537b4f5611da3b7cb3e7e22d5e9c
tkms0106 <siberianexpress1991@gmail.com>
2018-01-14 11:58:57 +0900

    [add]Eventのバリデーションに対するテストを追加

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    # このコードは慣習的に正しくない
+    @event = @user.events.build(title: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "user id should be present" do
+    @event.user_id = nil
+    assert_not @event.valid?
+  end
+
+  test "content should be present" do
+    @event.title = "   "
+    assert_not @event.valid?
+  end
 end
