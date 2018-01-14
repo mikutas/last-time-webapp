867c7f5856cac2649b608ada9a1c0e6fb7f84b85
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-01-14 10:08:40 +0900

    [clean]不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
-  get 'users/new'
-
   root 'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
-  get    '/signup',  to: 'users#new'
-  post   '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
+  get    '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-  get '/auth/:provider/callback', to: 'sessions#create'
 end
