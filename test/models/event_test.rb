cad30613e028e611b4ab5b27412c1dd9b3331cc7
tkms0106 <siberianexpress1991@gmail.com>
2018-01-14 14:12:46 +0900

    [add]OccurredDateが連動して作成・削除されることを確認するテストを追加

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -15,4 +15,13 @@ class EventTest < ActiveSupport::TestCase
     @event.title = "   "
     assert_not @event.valid?
   end
+
+  test "associated dates should be created and destroyed" do
+    @user.save
+    @event = @user.events.create!(title: "Lorem ipsum")
+    assert_equal @event.occurred_dates.count, 1
+    @user.destroy
+    assert_equal Event.count, 0
+    assert_equal OccurredDate.count, 0
+  end
 end
