788536e3e65988f46add9adec49a03d616cf1549
tkms0106 <siberianexpress1991@gmail.com>
2018-01-14 13:21:56 +0900

    [update]OccurredDateにインデックスを追加してマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180114022313) do
+ActiveRecord::Schema.define(version: 20180114041956) do
 
   create_table "authorizations", force: :cascade do |t|
     t.string "provider"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20180114022313) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_events_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "occurred_dates", force: :cascade do |t|
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_occurred_dates_on_created_at"
+    t.index ["event_id"], name: "index_occurred_dates_on_event_id"
   end
 
   create_table "users", force: :cascade do |t|
