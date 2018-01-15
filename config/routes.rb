fb93de0eaae6d4e6a52df380ed2b583b0b0ae960
tkms0106 <siberianexpress1991@gmail.com>
2018-01-15 21:06:19 +0900

    [clean]不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   get    '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users, only: [:show]
 end
