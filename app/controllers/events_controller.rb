135a401d8df5f91be56e9419b6f09bc86ac12394
tkms0106 <siberianexpress1991@gmail.com>
2018-01-19 17:58:24 +0900

    [add]イベント作成機能と削除機能を追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,21 @@
 class EventsController < ApplicationController
+  before_action :logged_in_user
 
   def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      flash[:success] = "Event created!"
+      redirect_to current_user
+    else
+      render 'static_pages/home'
+    end
   end
 
-  def edit
-  end
-
-  def update
+  def destroy
+    @event = current_user.events.find_by_id(params[:eid])
+    @event.destroy
+    flash[:success] = "Event deleted"
+    redirect_to current_user
   end
 
   def show
@@ -14,4 +23,10 @@ class EventsController < ApplicationController
     @event = Event.find(params[:eid])
     @occurred_dates = @event.occurred_dates.paginate(page: params[:page])
   end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:title)
+    end
 end
