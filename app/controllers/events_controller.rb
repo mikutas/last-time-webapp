9f0596abe30ca973eeb944b66ab4e19107d59f75
tkms0106 <siberianexpress1991@gmail.com>
2018-01-23 22:02:14 +0900

    [update]記述の統一

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,7 +20,7 @@ class EventsController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @event = Event.find(params[:eid])
+    @event = @user.events.find(params[:eid])
     @occurred_dates = @event.occurred_dates.paginate(page: params[:page])
   end
 
