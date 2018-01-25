d366dcd915ce542e014a09689a7cb733942b315e
tkms0106 <siberianexpress1991@gmail.com>
2018-01-25 23:04:10 +0900

    [update]最初のイベント発生日時をイベント自身の生成日時に合わせる
    イベントの生成日時はフォームから入力される

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,11 +2,10 @@ class Event < ApplicationRecord
   after_save :create_first_date
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  validates_presence_of :user_id
-  validates_presence_of :title
+  validates_presence_of :user_id, :title, :created_at
   has_many :occurred_dates, dependent: :delete_all
 
   def create_first_date
-    self.occurred_dates.create(created_at: Time.zone.now)
+    self.occurred_dates.create(created_at: self.created_at)
   end
 end
