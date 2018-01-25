c03cd2f607fb7764c80012dc4ff7d01ffd654780
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-01-25 23:14:37 +0900

    [update]Eventのcreated_atバリデーション追加に合わせて修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -54,7 +54,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "associated events should be destroyed" do
     @user.save
-    @user.events.create!(title: "Lorem ipsum")
+    @user.events.create!(title: "Lorem ipsum", created_at: Time.zone.now)
     assert_difference 'Event.count', -1 do
       @user.destroy
     end
