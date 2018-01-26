ef117ef062de0376f94dd8f925279765780f5b9f
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-01-26 21:48:50 +0900

    [update]Contactページの削除、Newsリンク削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
   get    '/login',   to: 'sessions#new'
   get    '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
