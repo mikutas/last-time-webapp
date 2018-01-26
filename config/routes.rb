03c68eb100c3225c4bbc10a1e284cea03f06f106
tkms0106 <siberianexpress1991@gmail.com>
2018-01-26 23:56:27 +0900

    [update]ログインページの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
-  get    '/login',   to: 'sessions#new'
   get    '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users, only: [:show]
