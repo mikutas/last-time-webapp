1dc7488fd90dc44a71e7b530ae0b0f76d543cb16
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-01-27 00:34:24 +0900

    [update]追加する日時をPOSTで受け取るように変更

--- app/controllers/occurred_dates_controller.rb
+++ app/controllers/occurred_dates_controller.rb
@@ -6,13 +6,13 @@ class OccurredDatesController < ApplicationController
     redirect_to root_url if @user.nil?
     @event = @user.events.find_by_id(params[:eid])
     redirect_to root_url if @event.nil?
-    @occurred_date = @event.occurred_dates.build(created_at: Time.zone.now)
+    @occurred_date = @event.occurred_dates.build(occurred_date_params)
     if @occurred_date.save
       flash[:success] = 'Event updated'
     else
       flash[:danger] = 'Event update failed'
     end
-    redirect_to current_user
+    redirect_to show_event_path(@user.id, @event.id)
   end
 
   def destroy
@@ -24,4 +24,10 @@ class OccurredDatesController < ApplicationController
     @occurred_date.destroy
     redirect_to show_event_path(uid, eid)
   end
+
+  private
+
+    def occurred_date_params
+      params.require(:occurred_date).permit(:created_at)
+    end
 end
