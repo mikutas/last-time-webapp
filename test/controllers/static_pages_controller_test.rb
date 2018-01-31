826d2b92605832a1af2b1ef78f34f2283b7a3c7f
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-01-31 19:23:13 +0900

    [fix]ページタイトル変更に伴うテストの修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,25 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @base_title = "When is the last time?"
+  end
+
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", @base_title
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | " + @base_title
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | " + @base_title
   end
 end
