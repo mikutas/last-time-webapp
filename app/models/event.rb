29ec8f476a6dd3489ebbdc64b52c1754d118199e
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-01-31 19:24:02 +0900

    [fix]並び順の修正
    およびcreate_first_dateのprivate化

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,13 @@
 class Event < ApplicationRecord
-  after_save :create_first_date
+  after_create :create_first_date
   belongs_to :user
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(updated_at: :desc) }
   validates_presence_of :user_id, :title, :created_at
   has_many :occurred_dates, dependent: :delete_all
 
-  def create_first_date
-    self.occurred_dates.create(created_at: self.created_at)
-  end
+  private
+
+    def create_first_date
+      self.occurred_dates.create(created_at: self.created_at)
+    end
 end
