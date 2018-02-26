5ee425ab736bb40f08e9e88a842f0a5ea8a34f6a
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-02-26 23:57:02 +0900

    [fix, update]destroyに正しいユーザーを要求するように修正、ルーティングの変更に追従

--- app/controllers/occurred_dates_controller.rb
+++ app/controllers/occurred_dates_controller.rb
@@ -1,33 +1,32 @@
 class OccurredDatesController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_user,   only: :destroy
 
   def create
-    @user = User.find_by_id(params[:id])
-    redirect_to root_url if @user.nil?
-    @event = @user.events.find_by_id(params[:eid])
-    redirect_to root_url if @event.nil?
+    @event = current_user.events.find_by_id(params[:occurred_date][:event_id])
     @occurred_date = @event.occurred_dates.build(occurred_date_params)
     if @occurred_date.save
       flash[:success] = 'Event updated.'
     else
       flash[:danger] = 'Event update failed.'
     end
-    redirect_to show_event_path(@user.id, @event.id)
+    redirect_to event_path(@event.id)
   end
 
   def destroy
-    @occurred_date = OccurredDate.find_by_id(params[:id])
-    redirect_to root_url if @occurred_date.nil?
     eid = @occurred_date.event_id
-    @event = Event.find_by_id(eid)
-    uid = User.find_by_id(@event.user_id)
     @occurred_date.destroy
-    redirect_to show_event_path(uid, eid)
+    redirect_to event_path(eid)
   end
 
   private
 
     def occurred_date_params
       params.require(:occurred_date).permit(:created_at)
+    end
+
+    def correct_user
+      @occurred_date = OccurredDate.find_by_id(params[:id])
+      redirect_to root_url unless @occurred_date.event.user == current_user
     end
 end
