8637b258641448e4cf8150d23eae93d3bced85b2
tkms0106 <siberianexpress1991@gmail.com>
2018-02-27 21:00:34 +0900

    [update]'/auth/:provider/callback'に対してパスを定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
-  get    '/auth/:provider/callback', to: 'sessions#create'
+  get    '/auth/:provider/callback', to: 'sessions#create', as: 'login'
   delete '/logout',  to: 'sessions#destroy'
   resources :users, only: [:show, :edit, :destroy]
   resources :events, only: [:show, :create, :destroy]
