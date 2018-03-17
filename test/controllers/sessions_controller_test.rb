d76502031337b030e5fa2407c02b3841232aed7f
tkms0106 <siberianexpress1991@gmail.com>
2018-03-17 14:39:48 +0900

    [add]認証情報が空だった場合のテストを追加

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should redirect create if auth is blank" do
+    get login_path('google_oauth2')
+    assert_redirected_to root_url
+  end
 end
