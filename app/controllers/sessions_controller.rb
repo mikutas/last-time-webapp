3543d7ef52da2fb77f2078fd0c0bbc43b49dc35d
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-03-17 14:40:35 +0900

    [fix]認証情報が空の場合の描画対象が存在しない問題を修正
    rootにリダイレクトするように変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       redirect_back_or user
     else
       flash.now[:danger] = 'Authorization failed.'
-      render 'new'
+      redirect_to root_url
     end
   end
 
