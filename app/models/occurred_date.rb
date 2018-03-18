94c67b9edc0d6be1176f61bc77ff8009a32d6bb6
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-03-18 20:22:34 +0900

    [clean]不要なメソッドの削除

--- app/models/occurred_date.rb
+++ app/models/occurred_date.rb
@@ -1,12 +1,5 @@
 class OccurredDate < ApplicationRecord
-  after_create :update_event
   belongs_to :event
   default_scope -> { order(created_at: :desc) }
   validates_presence_of :created_at
-
-  private
-
-    def update_event
-      self.event.update_attributes(updated_at: self.created_at)
-    end
 end
