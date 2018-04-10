65d08ee717f6c5825b7f1b52a387f97af27387b6
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-04-11 07:38:18 +0900

    [fix]イベント一覧が正しく取得できない問題を修正

--- app/models/occurred_date.rb
+++ app/models/occurred_date.rb
@@ -1,5 +1,17 @@
 class OccurredDate < ApplicationRecord
+  after_save :update_event_after_save
+  before_destroy :update_event_before_destroy
   belongs_to :event
   default_scope -> { order(occurred_at: :desc) }
   validates_presence_of :occurred_at
+
+  private
+
+    def update_event_after_save
+      self.event.update(occurred_at: self.event.occurred_dates.first.occurred_at)
+    end
+
+    def update_event_before_destroy
+      self.event.update(occurred_at: self.event.occurred_dates.where.not(id: self.id).first.occurred_at)
+    end
 end
