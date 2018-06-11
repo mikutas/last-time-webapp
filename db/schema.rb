8dfa99ca8f00f871b95d46f187379709fb8c0130
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-06-11 21:47:01 +0900

    [update]謎の更新
    別のマシンに開発環境を構築してdb:migrateしたところ、バージョンの日付部分にアンダースコアが入った

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411123819) do
+ActiveRecord::Schema.define(version: 2018_04_11_123819) do
 
   create_table "authorizations", force: :cascade do |t|
     t.string "provider"
