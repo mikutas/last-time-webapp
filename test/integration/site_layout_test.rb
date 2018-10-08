a155206d3dc5ec3c5f88bc5960cae660afd7e35f
tkms0106 <siberianexpress1991@gmail.com>
2018-10-08 17:43:34 +0900

    [clean]コメントアウトの削除

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,7 +6,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path
-    #assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
   end
 end
