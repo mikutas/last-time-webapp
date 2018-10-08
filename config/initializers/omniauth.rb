e7a4368980111c411a1c7ae41284b75bd6deb08b
tkms0106 <siberianexpress1991@gmail.com>
2018-10-08 17:28:43 +0900

    [clean]erb内ではダブルクオーテーションに統一

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-  Rails.application.credentials.google_oauth2[:key],
-  Rails.application.credentials.google_oauth2[:secret] unless Rails.env.test?
+  if Rails.env.production? || Rails.env.development?
+    provider :google_oauth2,
+             Rails.application.credentials.google_oauth2[:key],
+             Rails.application.credentials.google_oauth2[:secret]
+  elsif Rails.env.test?
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+      provider: 'google_oauth2',
+      uid: SecureRandom.uuid,
+      info: {
+        email: 'testuser@example.com'
+      }
+    )
+  end
 end
