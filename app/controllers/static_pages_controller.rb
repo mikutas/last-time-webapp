efe2552ee53202e05ea93dde63afbbd360ed817a
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-10-08 16:58:23 +0900

    [fix]Helpページ削除に伴う修正
    不要なテスト、ルーティングとアクションを削除

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,6 @@ class StaticPagesController < ApplicationController
     redirect_to user_events_path(current_user.id) if logged_in?
   end
 
-  def help
-  end
-
   def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/auth/:provider/callback', to: 'sessions#create', as: 'login'
   delete '/logout',  to: 'sessions#destroy'
