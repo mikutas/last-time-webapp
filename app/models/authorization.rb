91beec4a3d1b7dff2f36567b3665068afd887cdf
tkms0106 <siberianexpress1991@gmail.com>
2018-10-14 12:47:57 +0900

    [add]Authorizationのユニットテストを追加

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,14 +1,18 @@
 class Authorization < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id, :uid, :provider
-  validates_uniqueness_of :uid, uniqueness: {:scope => :provider}
+  validates_uniqueness_of :uid, uniqueness: { scope: :provider }
 
   def self.find_by_auth(auth)
+    return unless auth && auth.valid?
+
     find_by_provider_and_uid(auth['provider'], auth['uid'])
   end
 
-  def self.create_with_auth(auth)
+  def self.create_with_auth!(auth)
+    return unless auth && auth.valid?
+
     user ||= User.create_with_auth!(auth)
-    self.create!(:user => user, :uid => auth['uid'], :provider => auth['provider'])
+    create!(user: user, uid: auth['uid'], provider: auth['provider'])
   end
 end
