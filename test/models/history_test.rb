38c0385b3387a8b1eaa81d1910976d1d85ae0cc7
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-10-14 13:41:17 +0900

    [add]Historyのユニットテストを追加

--- test/models/history_test.rb
+++ test/models/history_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class OccurredDateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @event = events(:orange)
+    @new_history = @event.histories.build(occurred_at: Time.zone.now)
+  end
+
+  test 'occurred_at should be present' do
+    @new_history.occurred_at = nil
+    assert_not @new_history.valid?
+  end
+
+  test 'associated event should be updated when saved' do
+    @new_history.save
+    assert_equal @event.histories.first.occurred_at, @event.occurred_at
+  end
+
+  test 'associated event should be updated when destroyed' do
+    @new_history.save
+    second = @event.histories.second
+    @event.histories.first.destroy
+    assert_equal second.occurred_at, @event.occurred_at
+  end
 end
