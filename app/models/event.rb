f468378f728d03d8428e3c24a6a55de66a110c50
tkms0106 <siberianexpress1991@gmail.com>
2018-10-14 13:12:39 +0900

    [add]Eventのユニットテストを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,21 +1,17 @@
 class Event < ApplicationRecord
   after_create :create_first_date
   validates_presence_of :user_id, :title, :occurred_at
-  validates_uniqueness_of :title, uniqueness: {:scope => :user_id}
+  validates_uniqueness_of :title, uniqueness: { scope: :user_id }
   belongs_to :user
   has_many :histories, dependent: :delete_all
 
   private
 
     def create_first_date
-      self.histories.create(occurred_at: self.occurred_at)
+      histories.create(occurred_at: occurred_at)
     end
 
     def self.search(term)
-      if term
-        where(['title LIKE ?', "%#{term}%"])
-      else
-        all
-      end
+      term ? where(['title LIKE ?', "%#{term}%"]) : all
     end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,25 +3,41 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    @event = @user.events.build(title: "Lorem ipsum", occurred_at: Time.zone.now)
+    @event = @user.events.build(title: 'Lorem ipsum', occurred_at: Time.zone.now)
   end
 
-  test "user id should be present" do
+  test 'user id should be present' do
     @event.user_id = nil
     assert_not @event.valid?
   end
 
-  test "content should be present" do
-    @event.title = "   "
+  test 'title should be present' do
+    @event.title = '   '
     assert_not @event.valid?
   end
 
-  test "associated dates should be created and destroyed" do
+  test 'occurred_at should be present' do
+    @event.occurred_at = nil
+    assert_not @event.valid?
+  end
+
+  test "title should not be duplicated within the same user's event" do
+    @event.save
+    duplicated = @user.events.build(title: @event.title, occurred_at: Time.zone.now)
+    assert_not duplicated.valid?
+  end
+
+  test 'associated history should be created when new event is created' do
     assert_difference 'History.count', 1 do
       @event.save
     end
-    assert_difference 'History.count', -1 do
-      @event.destroy
+  end
+
+  test 'associated history should be deleted all when event is destroyed' do
+    event = events(:orange)
+    count = event.histories.count
+    assert_difference 'History.count', -count do
+      event.destroy
     end
   end
 end
