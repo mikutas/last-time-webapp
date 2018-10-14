62b2d64c654e4e8ccaa6dcb5af63684bcbf2a5ad
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-10-14 16:21:31 +0900

    [clean]不要なスペース削除、シンボル配列を%記法に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get    '/about',   to: 'static_pages#about'
+  get    '/about', to: 'static_pages#about'
   get    '/auth/:provider/callback', to: 'sessions#create', as: 'login'
-  delete '/logout',  to: 'sessions#destroy'
-  resources :users, only: [:edit, :destroy] do
-    resources :events, only: [:index, :edit, :update, :create, :destroy] do
-      resources :histories, only: [:index, :create, :destroy]
+  delete '/logout', to: 'sessions#destroy'
+  resources :users, only: %i[edit destroy] do
+    resources :events, only: %i[index edit update create destroy] do
+      resources :histories, only: %i[index create destroy]
     end
   end
 end
